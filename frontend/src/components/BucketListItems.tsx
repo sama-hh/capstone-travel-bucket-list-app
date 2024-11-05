cc60e650305ba82614c7d8f011be2089f93b7b0f
sama-hh <samahajimuradovah@gmail.com>
2024-11-05 16:49:58 +0100

    add design to items

--- frontend/src/components/BucketListItems.tsx
+++ frontend/src/components/BucketListItems.tsx
@@ -6,31 +6,28 @@ type BucketListItemsProps = {
 }
 const BucketListItems = ({bucketList}: BucketListItemsProps) => {
     return (
-        <Row className="justify-content-center mt-3">
+        <Row className="mt-3">
             {bucketList.map((item) => (
-                <Col key={item.id} sm={12} md={6} lg={4} className="mb-4">
+                <Col key={item.id} sm={12} md={6} lg={3} className="mb-4">
                     <Card className="bucket-card shadow-sm">
                         <Card.Body>
-                            <Card.Title>
+                            <Card.Title className="text-start fw-bold">
                                 {item.name}
                             </Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">{item.country}</Card.Subtitle>
-                            <Card.Text>
-                  <span className={`badge ${item.status === BucketListStatus.VISITED ? 'bg-success' : 'bg-warning'}`}>
-                    {item.status}
-                  </span>
+                            <Card.Subtitle className="mb-2 text-muted text-start">{item.country}</Card.Subtitle>
+                            <Card.Text className="text-start">
+                                <span className={`badge ${item.status === BucketListStatus.VISITED ? 'bg-success' : 'bg-warning'}` }>
+                                {item.status}
+                                </span>
                             </Card.Text>
-                            <div className="d-flex justify-content-between">
-                                <Button variant="primary" className="me-2">
-                                    Edit
-                                </Button>
-                                <Button variant="danger" className="me-2">
-                                    Remove
-                                </Button>
-                                <Button variant="success">
-                                    Mark as Visited
-                                </Button>
-                            </div>
+                                <div className="d-flex justify-content-end gap-2">
+                                    <Button variant="primary" className="btn-sm">
+                                        Edit
+                                    </Button>
+                                    <Button variant="danger" className="btn-sm">
+                                        Remove
+                                    </Button>
+                                </div>
                         </Card.Body>
                     </Card>
                 </Col>
