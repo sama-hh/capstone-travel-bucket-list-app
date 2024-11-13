68352078bbf2af1ac53d0331ff5eae94e053133f
sama-hh <samahajimuradovah@gmail.com>
2024-11-13 19:07:53 +0100

    fix login page test

--- backend/src/test/java/org/example/backend/controller/BucketListControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/BucketListControllerTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WithMockUser(username = "user")
 class BucketListControllerTest {
 
     @Autowired
